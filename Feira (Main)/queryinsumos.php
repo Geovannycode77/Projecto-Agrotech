c99a8a6c6ff69759add5a49dd75d2e6689fe6637
Geovannycode77 <jilopknilo1245@gmail.com>
2025-10-01 13:49:45 +0100

    cleaning

--- Feira (Main)/queryinsumos.php
+++
@@ -1,8 +0,0 @@
-<?php
-$stmt = $pdo->query("SELECT * FROM insumos ORDER BY nome");
-$insumos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($insumos as $insumo) {
-    echo "<p>{$insumo['nome']} - Categoria: {$insumo['categoria']} - Unidade: {$insumo['unidade']}</p>";
-}
-?>
--- README.md
+++
@@ -1,2 +0,0 @@
-# Projecto-Agrotech
-Criação de um sistema web de gestão de eficiência e sustentabilidade na criação de gado
