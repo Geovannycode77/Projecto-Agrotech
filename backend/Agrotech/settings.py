45aa48da413862129ea40879edab3a91d0193d14
Dkka20 <167699729+Dkka20@users.noreply.github.com>
2026-01-19 14:50:59 +0100

    Update settings.py

--- backend/Agrotech/settings.py
+++ backend/Agrotech/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'Agrotech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Agrotech',
+        'USER': 'postgres',
+        'PASSWORD': 'dkka',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
